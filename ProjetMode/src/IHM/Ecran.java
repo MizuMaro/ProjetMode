3ca8168a7012eefb2d74a38a36a5648c0c95d6eb
Remi DEJONGHE <remi.dejonghe@etudiant.univ-lille1.fr>
2015-11-06 16:43:42 +0100

    AjoutCubique

--- ProjetMode/src/IHM/Ecran.java
+++ ProjetMode/src/IHM/Ecran.java
@@ -18,6 +18,7 @@ public class Ecran {
 	private Obstacle ob3;
 	private Obstacle ob4;
 	private Obstacle ob5;
+	private Obstacle ob6;
 	private int compteurTouch=1;
 	
 	private Affichage affichage ;
@@ -44,6 +45,8 @@ public class Ecran {
 				fenetre.getHeight() - 470));
 		ob5 = new Obstacle(new Point(fenetre.getWidth() - 180,
 				fenetre.getHeight() - 600));
+		ob6 = new Obstacle(new Point(fenetre.getWidth() - 800,
+				fenetre.getHeight() - 150));
 
 		// ajout des obstacles dans la liste pour pouvoir les gerer
 		obstacles.add(ob1);
@@ -51,15 +54,18 @@ public class Ecran {
 		obstacles.add(ob3);
 		obstacles.add(ob4);
 		obstacles.add(ob5);
+		obstacles.add(ob6);
 
 		affichage = new Affichage(a, obstacles, compteurTouch);
 		fenetre.setContentPane(affichage);		
 
 		// differentes courbes
 		courbe(0.0009, -1, 500, a);
+		courbeCubique(-0.0000005, 0.1, 450, a);
 		courbe(0.0008, -1.01, 500, a);
 		courbe(0.00077, -1.05, 500, a);
 		courbe(0.0007, -1.05, 500, a);
+		courbeCubique(0.000001, 0.001, 450, a);
 		courbe(0.0005, -1, 500, a);
 
 	}
@@ -105,6 +111,85 @@ public class Ecran {
 			y2 = (int) ((int) (a*2*x2+b)*(x2-a)+(a * Math.pow(x2, 2) + b * x2 + c));
 			// courbe ax2+bx+c
 			y = (int) (a * Math.pow(x, 2) + b * x + c);
+			
+			// System.out.println(y);
+			// nouvelle position de l'oiseau
+			o.setC(x, y);
+			//o.setC2(x2, y2);
+			
+			// utiliser timer
+			long now = System.currentTimeMillis();
+			long time = now + 10;
+			
+			while (now < time) {
+				now = System.currentTimeMillis();
+			}
+
+			// test si un obstacle est touche
+			for (Obstacle ob : obstacles) {
+				if (this.a.getC().getX() > ob.getC().getX() - Constantes.TAILLE_OBSTACLES
+						&& this.a.getC().getX() < ob.getC().getX() + Constantes.TAILLE_OBSTACLES
+						&& this.a.getC().getY() > ob.getC().getY() - Constantes.TAILLE_OBSTACLES
+						&& this.a.getC().getY() < ob.getC().getY() + Constantes.TAILLE_OBSTACLES) {
+					affichage.setCollision(true);
+					touch = true;
+					compteurTouch++;
+					ob.setActif(false);
+					affichage.repaint();
+					now = System.currentTimeMillis();
+					time = now + 2000;
+					
+					while (now < time) {
+						now = System.currentTimeMillis();
+					}
+				}
+				if (touch == false)
+				o.setC2(x2, y2);
+			}
+			
+			affichage.repaint();
+		}
+	}
+	
+void courbeCubique(double a, double b, double c, Oiseau o) throws InterruptedException {
+		
+		affichage.setCollision(false);
+		
+		if(Constantes.TRAJECTOIRE_UNIQUE)
+			o.effacerTrajectoire();
+			o.effacerTrajectoireTangeante();
+			
+		affichage.repaint();
+		o.setC(50, 450);
+		o.setC2(100, 430);
+		affichage.repaint();
+		
+		long premier = System.currentTimeMillis();
+		long deuxieme = premier+1000;
+		
+		while(premier<deuxieme){
+			premier=System.currentTimeMillis();
+		}
+		
+		int y;
+		int x = 50;
+		int x2 = 50+50;
+		int y2 =0;
+		boolean touch = false;
+		
+		long timeLancement = System.currentTimeMillis();
+		long timeFin = timeLancement + 15000;
+		
+		while (x <= 800 * 7 && !touch && timeLancement<timeFin) {
+			
+			timeLancement = System.currentTimeMillis();
+			// plus ou moins loin
+			x += 4;
+			//pour tangente
+			x2 = x+50;
+			y2 = (int) ((int) (a*3*Math.pow(x2,2)+b)*(x2-a)+(a * Math.pow(x2, 3) + b * x2 + c));
+			// courbe ax2+bx+c
+			y = (int) (a * Math.pow(x, 3) + b * x + c);
 			
 			// System.out.println(y);
 			// nouvelle position de l'oiseau
