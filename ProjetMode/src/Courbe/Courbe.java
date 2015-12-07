43ab08ce83859399d2c5d762ff8c6926b57a7465
Alystrasz <raes.remy@gmail.com>
2015-12-07 13:54:35 +0100

    Passage à Java 1.7

--- ProjetMode/src/Courbe/Courbe.java
+++ ProjetMode/src/Courbe/Courbe.java
@@ -13,126 +13,126 @@ import Obstacles.ObstacleMouvant;
 public class Courbe {
 
 	private boolean run = true;
-	
-	public Courbe(double a, double b, double c, Oiseau o, Affichage affichage, ArrayList<Obstacle> obstacles){
-		
+
+	public Courbe(final double a, final double b, final double c, final Oiseau o, final Affichage affichage, final ArrayList<Obstacle> obstacles){
+
 		final Timer timer = new Timer(); 
 		TimerTask timerTask = new TimerTask(){	
-			
+
 			public void run()  { 
 
 				if(run){
-				run = false;
-				if(Constantes.TRAJECTOIRE_UNIQUE){
-					o.effacerTrajectoire();
-					o.effacerTrajectoireTangeante();
-				}
-
-				affichage.repaint();
-
-				
-				o.setC(50, 450);
-				o.setC2(100, 430);
-
-				
-				affichage.repaint();
-
-				long premier = System.currentTimeMillis();
-				long deuxieme = premier;
-				while(premier<deuxieme){
-					premier=System.currentTimeMillis();
-				}
-
-				int y;
-				int x = 50;
-				int x2 = 50+50;
-				int y2 =0;
-				boolean touch = false;
-				boolean sol = false;
-				boolean limites = false;
-
-				long timeLancement = System.currentTimeMillis();
-				long timeFin = timeLancement + 15000;
-
-				while (x <= 800 * 7 && !touch && timeLancement<timeFin) {
-
-					// collision avec le sol
-					sol = (o.getC().y >= Constantes.HAUTEUR_SOL - Constantes.TAILLE_OISEAU);
-					// sortie de l'ecran
-					limites = (o.getC().x > Constantes.TAILLE_ECRAN[0] - Constantes.TAILLE_OISEAU || o.getC().x < 0);
-
-					timeLancement = System.currentTimeMillis();
-					// plus ou moins loin
-					x += 4;
-					//pour tangente
-					x2 = x+50;
-					y2 = (int) ((int) (a*2*x2+b)*(x2-a)+(a * Math.pow(x2, 2) + b * x2 + c));
-					// courbe ax2+bx+c
-					y = (int) (a * Math.pow(x, 2) + b * x + c);
-
-					// System.out.println(y);
-					// nouvelle position de l'oiseau
-					o.setC(x, y);
-					//o.setC2(x2, y2);
-
-					// utiliser timer
-					long now = System.currentTimeMillis();
-					long time = now + 10;
-
-					while (now < time) {
-						now = System.currentTimeMillis();
+					run = false;
+					if(Constantes.TRAJECTOIRE_UNIQUE){
+						o.effacerTrajectoire();
+						o.effacerTrajectoireTangeante();
 					}
 
-					for (Obstacle ob : obstacles) {
-
-						//on bouge les obstacles qui se déplacent
-						if(ob instanceof ObstacleMouvant && ob.isActif()){
-							((ObstacleMouvant)ob).moveX();
-							((ObstacleMouvant)ob).moveY();
-						}
-
-						if(ob.isActif() && affichage.distance(o.getC().getX(), o.getC().getY(), 
-								ob.getC().getX(), ob.getC().getY()) <= (Constantes.TAILLE_OISEAU/2 + Constantes.TAILLE_OBSTACLES/2) 
-								// collision avec le sol ?
-								|| sol 
-
-								//sorti de l'ecran ?
-								|| limites){
-
-							// implementer la collision avec les obstacles carres
-
-							affichage.setCollision(true);
-							touch = true;
-							if(sol==false && limites==false){
-								ob.setActif(false);
-							}
-							affichage.repaint();
-							now = System.currentTimeMillis();
-							time = now + 2000;
-							sol=false;
-							limites=false;
-
-							while (now < time) {
-								now = System.currentTimeMillis();
-							}
-						}
-						if (touch == false)
-							o.setC2(x2, y2);
-					}
-
-					affichage.setCollision(false);
-					if(touch){
-						o.setC(Constantes.COORDONNEES_ORIGINE.x, Constantes.COORDONNEES_ORIGINE.y);
-						o.setC2(o.getC().x+50, o.getC().y);	
-					}
-					
 					affichage.repaint();
-				}
+
+
+					o.setC(50, 450);
+					o.setC2(100, 430);
+
+
+					affichage.repaint();
+
+					long premier = System.currentTimeMillis();
+					long deuxieme = premier;
+					while(premier<deuxieme){
+						premier=System.currentTimeMillis();
+					}
+
+					int y;
+					int x = 50;
+					int x2 = 50+50;
+					int y2 =0;
+					boolean touch = false;
+					boolean sol = false;
+					boolean limites = false;
+
+					long timeLancement = System.currentTimeMillis();
+					long timeFin = timeLancement + 15000;
+
+					while (x <= 800 * 7 && !touch && timeLancement<timeFin) {
+
+						// collision avec le sol
+						sol = (o.getC().y >= Constantes.HAUTEUR_SOL - Constantes.TAILLE_OISEAU);
+						// sortie de l'ecran
+						limites = (o.getC().x > Constantes.TAILLE_ECRAN[0] - Constantes.TAILLE_OISEAU || o.getC().x < 0);
+
+						timeLancement = System.currentTimeMillis();
+						// plus ou moins loin
+						x += 4;
+						//pour tangente
+						x2 = x+50;
+						y2 = (int) ((int) (a*2*x2+b)*(x2-a)+(a * Math.pow(x2, 2) + b * x2 + c));
+						// courbe ax2+bx+c
+						y = (int) (a * Math.pow(x, 2) + b * x + c);
+
+						// System.out.println(y);
+						// nouvelle position de l'oiseau
+						o.setC(x, y);
+						//o.setC2(x2, y2);
+
+						// utiliser timer
+						long now = System.currentTimeMillis();
+						long time = now + 10;
+
+						while (now < time) {
+							now = System.currentTimeMillis();
+						}
+
+						for (Obstacle ob : obstacles) {
+
+							//on bouge les obstacles qui se déplacent
+							if(ob instanceof ObstacleMouvant && ob.isActif()){
+								((ObstacleMouvant)ob).moveX();
+								((ObstacleMouvant)ob).moveY();
+							}
+
+							if(ob.isActif() && affichage.distance(o.getC().getX(), o.getC().getY(), 
+									ob.getC().getX(), ob.getC().getY()) <= (Constantes.TAILLE_OISEAU/2 + Constantes.TAILLE_OBSTACLES/2) 
+									// collision avec le sol ?
+									|| sol 
+
+									//sorti de l'ecran ?
+									|| limites){
+
+								// implementer la collision avec les obstacles carres
+
+								affichage.setCollision(true);
+								touch = true;
+								if(sol==false && limites==false){
+									ob.setActif(false);
+								}
+								affichage.repaint();
+								now = System.currentTimeMillis();
+								time = now + 2000;
+								sol=false;
+								limites=false;
+
+								while (now < time) {
+									now = System.currentTimeMillis();
+								}
+							}
+							if (touch == false)
+								o.setC2(x2, y2);
+						}
+
+						affichage.setCollision(false);
+						if(touch){
+							o.setC(Constantes.COORDONNEES_ORIGINE.x, Constantes.COORDONNEES_ORIGINE.y);
+							o.setC2(o.getC().x+50, o.getC().y);	
+						}
+
+						affichage.repaint();
+					}
 				}else{
 					timer.cancel();
 				}
 			}
-			
+
 		};
 		//intervalle de temps entre chaque repaint
 		timer.scheduleAtFixedRate(timerTask,0,2000);
