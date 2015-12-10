fe50ffc1913e0143c9622fd6a61624579547c1c1
MizuMaro <lil_hasni@hotmail.fr>
2015-12-10 21:42:50 +0100

    Ajout du controller + Mise a jour du main (mvc)

---
+++ ProjetMode/src/Controller/Controller.java
@@ -0,0 +1,48 @@
+package Controller;
+
+import java.util.ArrayList;
+
+import Model.Model;
+import Obstacles.Obstacle;
+import Vue.Vue;
+
+public class Controller {
+	Model m ;
+	Vue v= null;
+	public Controller(Model m) {
+		this.m=m;
+	}
+	public void setPositionOiseau(int x , int y){
+		m.setPositionOiseau(x,y);
+		}
+	public void setPositionOiseauC2(int x , int y){
+		m.setPositionOiseauC2(x,y);
+	}
+
+	public void compteurTouchInc(){
+		m.setCompteurTouch(m.getCompteurTouch()+1);
+	}
+	public void setObstacles(ArrayList<Obstacle> obstacles) {
+		this.m.setObstacles(obstacles);
+	}
+	public void repaint(){
+		this.m.getAffichage().repaint();
+	}
+	
+	public void addVue(Vue v){
+		this.v = v;
+	}
+	public void initObstacles(){
+		this.m.initObstacles();
+	}
+	public void initAffichage(){
+		this.m.initAffichage();
+	}
+	public void vol(){
+		this.m.vol(true);
+	}
+	public void volFini(){
+		this.m.vol(false);
+	}
+	
+}
--- ProjetMode/src/Start/Lancer.java
+++ ProjetMode/src/Start/Lancer.java
@@ -1,18 +1,28 @@
 package Start;
+
+import Controller.Controller;
 import Element.Ecran;
+import Model.Model;
+import Vue.Vue;
 
 /**
- * La classe Lancer est le point d'entree du programme, celle-ci contenant la methode main.
- * @author Remy
+ * La classe Lancer est le point d'entree du programme, celle-ci contenant la
+ * methode main.
+ * 
+ * @author Omar
  *
  */
 public class Lancer {
+	public Lancer() {
+		Model model = new Model();
+		Controller control = new Controller(model);
+		Vue vue = new Vue(model, control);
+		control.addVue(vue);
+	}
 
-	
-	
 	public static void main(String[] args) throws InterruptedException {
-		new Ecran();
+		new Lancer();
+		//new Ecran();
 	}
 
 }
- 
